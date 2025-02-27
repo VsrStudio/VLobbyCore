5e1fbbad6329fa0e8c07506a57707a54d009d887
XOneFG <117354752+VsrStudio@users.noreply.github.com>
2025-02-27 10:20:53 +0700

    Update UI.php

--- src/LC/ui/UI.php
+++ src/LC/ui/UI.php
@@ -155,19 +155,88 @@ class UI {
         $form->sendToPlayer($player);
     }
 
-    public function getInfo(Player $player){
-        $form = new SimpleForm(function(Player $player, int $data = null){
-            if($data === null){
-                return true;
-            }
-            switch($data){
-                case 0:
-                    break;
+    public function openProfileForm(Player $player): void {
+        $form = new SimpleForm(function (Player $player, ?int $data): void {
+            if ($data !== null) {
+                // NOOP
             }
         });
-        $form->setTitle(MG::BLUE .$this->plugin->getConfig()->get("InfoTitle"));
-        $form->setContent(MG::RED . $this->plugin->getConfig()->get("Info"));
-        $form->addButton("§0Black");
-        $form->sendToPlayer($player);
+
+        libEco::myMoney($player,
+            function(float $money) use ($player, $form): void {
+                $name = $player->getName();
+                $rank = $this->getRank($player);
+                $expLevel = $player->getXpManager()->getXpLevel();
+                $date = date("d/m/Y H:i:s");
+                $ping = $this->getPing($player);
+                $firstPlayedDate = $this->getFormattedFirstPlayedDate($player);
+                $coords = $this->getCoords($player);
+
+                $form->setTitle(TF::BOLD . TF::RED . "YOUR PROFILE");
+                $content = "This is your profile on the server:\n\n" .
+                TF::GRAY . $date . "\n" .
+                TF::WHITE . "Name : " . TF::GREEN . $name . "\n" .
+                TF::WHITE . "Rank : " . TF::GREEN . $rank . "\n" .
+                TF::WHITE . "Money : " . TF::GREEN . $money . "\n" .
+                TF::WHITE . "Ping : " . TF::GREEN . $ping . "\n" .
+                TF::WHITE . "Experience : " . TF::GREEN . $expLevel . "\n" .
+                TF::WHITE . $coords . "\n" .
+                TF::WHITE . "First Join : " . TF::GREEN . $firstPlayedDate;
+
+                $form->setContent($content);
+                $form->addButton(TF::BOLD . TF::RED . "EXIT", 0, "textures/blocks/barrier");
+                $form->sendToPlayer($player);
+            });
+    }
+
+    private function getRank(Player $player): ?string {
+        $purePerms = $this->getPlugin("PurePerms");
+        $ranksystem = $this->getPlugin("RankSystem");
+
+        if ($purePerms !== null) {
+            $group = $purePerms->getUserDataMgr()->getGroup($player);
+            return $group !== null ? $group->getName() : null;
+        } elseif ($ranksystem !== null) {
+            $session = $ranksystem->getSessionManager()->get($player);
+            $rankNames = [];
+
+            foreach ($session->getRanks() as $rank) {
+                $rankNames[] = $rank->getName();
+            }
+
+            return implode(', ', $rankNames);
+        }
+
+        return null;
+    }
+
+    private function getPlugin(string $name) {
+        return $this->getServer()->getPluginManager()->getPlugin($name);
+    }
+
+    private function getFormattedFirstPlayedDate(Player $player): string {
+        $timestamp = (int)($player->getFirstPlayed() / 1000);
+        $formattedDate = date("F j, Y H:i:s", $timestamp);
+        return $formattedDate . " WIB";
+    }
+
+    private function getCoords(Player $player): string {
+        $x = (int) $player->getPosition()->getX();
+        $y = (int) $player->getPosition()->getY();
+        $z = (int) $player->getPosition()->getZ();
+
+        $position = TF::WHITE . "Position : " . TF::GREEN . $x . " " . $y . " " . $z . TF::RESET;
+
+        return $position;
+    }
+
+    private function getPing(Player $player): ?int {
+        $ping = $player->getNetworkSession()->getPing();
+        return $ping;
+    }
+
+    private function getWorldName(Player $player) {
+        $worldName = $player->getWorld()->getProvider()->getWorldData()->getName();
+        return $worldName;
     }
 }
