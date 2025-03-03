3422ce44bd01c5267cae3f80d3fdd1e6bd78b7a4
XOneFG <117354752+VsrStudio@users.noreply.github.com>
2025-03-03 19:24:51 +0700

    Create Utils.php

---
+++ src/VsrStudio/VLC/libs/libEco/Utils/Utils.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace VsrStudio\VLC\libs\libEco\Utils;
+
+final class Utils {
+
+    public function __construct(){
+        //NOTHING
+    }
+
+	public const ECONOMYAPI = "EconomyAPI";
+	
+	public const BEDROCKECONOMYAPI = "BedrockEconomyAPI";
+}
